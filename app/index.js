b236e1226cd745ddd71f12027e0deae0488a0742
Priyanka K U <priyanka.ku@gmail.com>
2020-03-30 15:08:26 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import App from './components/App'
+console.log(App);
+  ReactDOM.render(
+    <App />, document.getElementById('app')
+);
